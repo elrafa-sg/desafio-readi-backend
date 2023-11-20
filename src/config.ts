0583c07b4a0122971d5d9b37305a468c177a7abf
Rafael Gomes <silveiragomesr@gmail.com>
2023-11-20 00:44:29 -0300

    fix: adiciona variaveis usadas na autenticacao

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,6 @@ require('dotenv').config();
 
 export const API_PORT = process.env.PORT || '3000'
 export const API_HOST = process.env.HOSTNAME || 'localhost'
+
+export const ACCESS_EXPIRES = '1d'
+export const ACCESS_SECRET = 'quarentaedois'
