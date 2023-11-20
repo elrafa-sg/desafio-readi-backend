af447046816d11e28539f8dd924b3d8cae5eef02
Rafael Gomes <silveiragomesr@gmail.com>
2023-11-19 23:30:11 -0300

    chore: adiciona seed para criar usuarios de teste no banco

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+async function seedDatabase () {
+    await prisma.usuario.createMany({
+        data: [
+            { email: 'cliente01@email.com', senha: 'senha01', role: 'CLIENTE' },
+            { email: 'cliente02@email.com', senha: 'senha02', role: 'CLIENTE' },
+            { email: 'operador@email.com', senha: 'senha03', role: 'OPERADOR' },
+            { email: 'administrador@email.com', senha: 'senha04', role: 'ADMINISTRADOR' }
+        ]
+    })
+}
+
+seedDatabase()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
