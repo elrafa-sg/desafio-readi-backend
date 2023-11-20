87a704bb985a2950f00cd09df52b412e530560e8
Rafael Gomes <silveiragomesr@gmail.com>
2023-11-20 16:11:16 -0300

    feat: adiciona versao basica das rotas de solicitacao

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import usuarioRoutes from './usuarioRouter'
+import solicitacaoRoutes from './solicitacaoRouter'
 
 const router = express.Router()
 
 router.use('/usuario', usuarioRoutes)
+router.use('/solicitacao', solicitacaoRoutes)
 
 export default router
